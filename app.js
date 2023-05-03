1c7680a53c98e837cce205c4b464cead3adc8337
hao0516hsu <chhsu0516@gmail.com>
2023-05-04 01:05:05 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const port = 3000
 const exphbs = require('express-handlebars')
 // 載入Mongoose
 const mongoose = require('mongoose')
-// 載入JSON檔改成載入種子資料
-const Restaurant = require('./models/restaurant')
 // 載入Body-parser
 const bodyParser = require('body-parser')
 // 載入Method-override
 const methodOverride = require('method-override')
+// 引入router
+const routes = require('./routes/index')
 // dotenv設定
 if (process.env.MONGODB_URI !== 'production') {
   require('dotenv').config()
@@ -41,108 +41,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // method-override設定
 app.use(methodOverride('_method'))
 
-// 設定首頁的路由
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
-})
-
-// 設定查詢頁的路由
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  // 若空白搜尋，轉成首頁的路由
-  if (!keyword.trim()) {
-    return res.redirect("/")
-  }
-  // mongoose查詢: 用or連接兩個查詢條件，i為不分大小寫
-  Restaurant.find({
-    $or: [
-      { name: { $regex: new RegExp(keyword, 'i') } },
-      { category: { $regex: new RegExp(keyword, 'i') } }
-    ]
-  })
-    .lean()
-    .then(restaurants => res.render('index', { restaurants, keyword }))
-    .catch(error => console.log(error))
-})
-
-// 設定新增功能的路由 (NOTE: 新增頁要在SHOW之前)
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-
-// 設定送出新增的路由
-app.post('/restaurants', (req, res) => {
-  const restaurant = req.body
-  return Restaurant.create({
-    id: restaurant.restaurant_id,
-    name: restaurant.name,
-    name_en: restaurant.name_en,
-    category: restaurant.category,
-    image: restaurant.image,
-    location: restaurant.location,
-    phone: restaurant.phone,
-    google_map: restaurant.google_map,
-    rating: restaurant.rating,
-    description: restaurant.description
-  })
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-// 設定說明頁的路由
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant_id = req.params.restaurant_id
-
-  Restaurant.findById(restaurant_id)
-    .lean()
-    .then(restaurant => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-// 設定編輯頁的路由
-app.get('/restaurants/:restaurant_id/edit', (req, res) => {
-  const restaurant_id = req.params.restaurant_id
-
-  Restaurant.findById(restaurant_id)
-    .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-// 設定送出編輯的路由
-app.put('/restaurants/:restaurant_id/', (req, res) => {
-  const restaurant_id = req.params.restaurant_id
-
-  return Restaurant.findById(restaurant_id)
-    .then(restaurant => {
-      restaurant.id = req.body.restaurant_id
-      restaurant.name = req.body.name
-      restaurant.name_en = req.body.name_en
-      restaurant.category = req.body.category
-      restaurant.image = req.body.image
-      restaurant.location = req.body.location
-      restaurant.phone = req.body.phone
-      restaurant.google_map = req.body.google_map
-      restaurant.rating = req.body.rating
-      restaurant.description = req.body.description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${restaurant_id}`))
-    .catch(error => console.log(error))
-})
-
-// 設定刪除的路由
-app.delete('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant_id = req.params.restaurant_id
-
-  return Restaurant.findById(restaurant_id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// route設定
+app.use(routes)
 
 // 設定啟動伺服器相關
 app.listen(port, () => {
