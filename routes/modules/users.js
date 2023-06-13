e738ad25590be3214e6ce3d4bf814ff02a1a4919
hao0516hsu <chhsu0516@gmail.com>
2023-06-13 18:18:49 +0800

    feat: add user model and user registeration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // Login Page
 router.get('/login', (req, res) => {
@@ -13,7 +14,21 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res) => { })
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
 
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('此信箱已被註冊！')
+        res.render('register', { name, email, password, confirmPassword })
+      } else {
+        return User.create({ name, email, password })
+          .then(() => res.redirect('/users/login'))
+          .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router
